b6ba3c846b31d5a38ff84005c6aa769228f1eacf
Alan Bruno Ferreira da Silva <alanjjr83@gmail.com>
2025-08-22 18:54:18 -0300

    04: commit payload initial config

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,14 +1,8 @@
-import { Button } from '@/components/ui/button'
-import { Checkbox } from '@/components/ui/checkbox'
-import { Input } from '@/components/ui/input'
-import { Progress } from '@/components/ui/progress'
-import { Textarea } from '@/components/ui/textarea'
-
 export default function Home() {
   return (
     <div className="p-4">
       <div className="flex flex-col gap-y-4">
-        <div>
+        {/* <div>
           <Button variant="elevated">I am a button</Button>
         </div>
         <div>
@@ -22,7 +16,7 @@ export default function Home() {
         </div>
         <div>
           <Checkbox />
-        </div>
+        </div> */}
       </div>
     </div>
   )
